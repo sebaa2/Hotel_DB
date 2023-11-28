504dedf084addfd74491e7157b2cc81a3fdea396
darioalonso07 <130076936+darioalonso07@users.noreply.github.com>
2023-11-28 15:46:42 -0400

    Update forms.py

---
+++ HotelDB/CRM/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Reserva
+
+class ReservaForms(forms.ModelForm):
+    class Meta:
+        model = Reserva
+        fields = [ 'fecha_chekin', 'fecha_chekout', 'estado_reserva', 'hotel_idhotel', 'cliente_idcliente']
+        labels = {
+            'fecha_chekin' : 'Fecha registrarse: ',
+            'fecha_chekout' : 'Fecha chekout: ',
+            'estado_reserva' : 'Estado reserva: ',
+            'hotel_idhotel' : 'Hotel: ',
+            'cliente_idcliente' : 'Cliente: ',
+        }
+        widgets = {
+            'fecha_chekin' : forms.TimeInput(attrs={'class': 'form-control'}),
+            'fecha_chekout' : forms.TimeInput(attrs={'class': 'form-control'}),
+            'estado_reserva' : forms.TextInput(attrs={'class': 'form-control'}),
+            'hotel_idhotel' : forms.Select(attrs={'class': 'form-control'}),
+            'cliente_idcliente' : forms.Select(attrs={'class': 'form-control'})
+        }
