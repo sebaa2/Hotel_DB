a4d06bae9e14f03aa8ca034286c91578c8fc1573
hcamposxx <130091365+hcamposxx@users.noreply.github.com>
2023-12-08 20:59:34 -0300

    Update urls.py

--- HotelDB/HotelDB/urls.py
+++ HotelDB/HotelDB/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for HotelDB project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from CRM.views import listar_reserva, listar_cliente, listar_hotel, listar_empleados, listar_habitacion, AgregarCliente, logout, registrarCliente, crearReserva, login_view, login_
-
+#direcciones del login y registro
+from reservas.views import principal, registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +16,8 @@ urlpatterns = [
     path('agregarclientes/', AgregarCliente),
     path('login/',login_view, name="login"),
     path('logout/',logout, name="logout"),
+    #estas son las urls de pag principal y forms login
+    path('', principal, name='principal'),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('registro/', registro, name='registro'),
 ]
